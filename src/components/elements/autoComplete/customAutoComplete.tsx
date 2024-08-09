0739a20e21d533b9ee6738ab6d2a39e529c53359
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-09 11:12:46 +0330

    new feature:create auto compelete component + formik

---
+++ src/components/elements/autoComplete/customAutoComplete.tsx
@@ -0,0 +1,90 @@
+import { axiosGet } from "@/configs/httpService/httpService";
+import { CustomAutoCompleteProps } from "@/types/autoComplete";
+import { Autocomplete, CircularProgress, TextField } from "@mui/material";
+import { useState } from "react";
+
+
+export const CustomAutoComplete = (props:CustomAutoCompleteProps) => {
+  const {
+    url = '',
+    optionLabel = 'label',
+    placeholder,
+    disabled,
+    error,
+    helperText,
+    querySearch='',
+    onChange,
+  } = props;
+
+  const [open, setOpen] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [options, setOptions] = useState<any[]>([]);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axiosGet({
+        url,
+      });
+      setOptions(response);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+  const handleClose = () => setOpen(false);
+  const handleOpen = () => {
+    setOpen(true);
+    if (options.length==0) {
+      fetchData();
+    }
+  };
+
+  return (
+    <div className="w-full">
+      <Autocomplete
+        {...props}
+        open={open}
+        onOpen={handleOpen}
+        onClose={handleClose}
+        // onInputChange={(_event, newInputValue) => {
+        //   querySearch!='' && inputHandler(newInputValue);
+        // }}
+        options={Array.isArray(options) ? options : [options]}
+        getOptionLabel={(option: Record<string, any>) =>
+          option[optionLabel] ? option[optionLabel] : ""
+        }
+        fullWidth
+        size="small"
+        disabled={disabled}
+        loading={isLoading}
+        onChange={(_event, newValue): void => {
+          onChange?.(newValue);
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            placeholder={placeholder}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {isLoading ? (
+                    <CircularProgress color="inherit" size={20} />
+                  ) : null}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }}
+          />
+        )}
+      />
+      {error ? <helperText>{helperText}</HelperText> : null}
+    </div>
+  );
+};
+
