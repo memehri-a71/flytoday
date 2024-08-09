ab6ebdf7fefdfd4a8d52564d20b8ad07935d356f
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-09 11:37:47 +0330

    new feature:create custom button + type

---
+++ src/components/elements/button/customButton.tsx
@@ -0,0 +1,39 @@
+import { CustomButtonType } from "@/types/button";
+import { Button, CircularProgress } from "@mui/material";
+import { ReactNode } from "react";
+
+
+export const CustomButton = (props: CustomButtonType) => {
+  const {
+    children,
+    type = "submit",
+    variant = "contained",
+    onClick,
+    color = "primary",
+    isLoading, isFullWidth,
+    disabled
+  } = props;
+  return (
+    <Button
+      type={type}
+      variant={variant}
+      fullWidth={isFullWidth}
+      color={color}
+      onClick={onClick}
+      disableRipple
+      disabled={disabled}
+    >
+      <div className="flex gap-2 items-center">
+        {children}
+        {isLoading ? (
+          <CircularProgress
+            size="20px"
+            sx={{
+              color: "white",
+            }}
+          />
+        ) : null}
+      </div>
+    </Button>
+  );
+};
---
+++ src/types/button.d.ts
@@ -0,0 +1,10 @@
+export interface CustomButtonType {
+    children: ReactNode
+    type: "submit" | "button",
+    variant: "contained" | "outlined" | "text",
+    color: "primary",
+    isLoading: boolean
+    isFullWidth: boolean
+    disabled: boolean
+    onClick: () => void
+}
