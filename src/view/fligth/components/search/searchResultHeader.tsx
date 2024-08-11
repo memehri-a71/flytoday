5ffcb421b977bbd7bd727af4bd35063aff1aaa38
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-11 09:17:47 +0330

    new feature:create select for sorting  component

--- src/view/fligth/components/search/searchResultHeader.tsx
+++ src/view/fligth/components/search/searchResultHeader.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import { SortResult } from '../sortResult'
+import { CustomButton } from '@/components/elements/button/customButton'
 
 
 export const SearchResultHeader = () => {
     return (
-        <div className='flex justify-between'>
-            <div>
-                <p>بلیط هواپیمای تهران به استانبول</p>
+        <div className='flex max-sm:flex-col justify-between'>
+            <div className='sm:hidden flex gap-2 py-5'>
+                <CustomButton type='button' variant='contained' isFullWidth >
+                    <div className='flex items-center'>
+                        <p>فیلتر کردن</p>
+                    </div>
+                </CustomButton>
+                <CustomButton type='button' variant='contained' isFullWidth>
+                    <div className='flex items-center'>
+                        <p>مرتب‌سازی</p>
+                    </div>
+                </CustomButton>
+            </div>
+            <div >
+                <p className='text-xl font-bold'>بلیط هواپیمای تهران به استانبول</p>
                 <p>پرواز یافت شد . سه‌شنبه، ۱۲ اردیبهشت ۱۴۰۰</p>
             </div>
-            <SortResult />
+            <div className='max-sm:hidden'>
+                <SortResult />
+            </div>
         </div>
     )
 }
--- src/view/fligth/components/sortResult/index.tsx
+++ src/view/fligth/components/sortResult/index.tsx
@@ -1,7 +1,19 @@
+import { CustomSelect } from '@/components/elements/select/customSelect'
 import React from 'react'
 
 export const SortResult = () => {
+  const optionSort = [
+    { valeu: "priceMax", label: "گرانترین" },
+    { valeu: "priceMin", label: "ارزانترین" },
+    { valeu: "timeEarlier", label: "زودترین" },
+    { valeu: "timeLAter", label: "دیرترین" }
+  ]
   return (
-    <div>SortResult</div>
+   <div className='flex items-center gap-3'>
+       <p>مرتب‌سازی</p>
+    <div className='w-44'>
+      <CustomSelect options={optionSort}  />
+    </div>
+   </div>
   )
 }
