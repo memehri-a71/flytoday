3e9f5940a4cffe1e3200b713d4379e25bdc92d7e
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-13 04:18:55 +0330

    new feature:add image to flight page

--- src/view/fligth/index.tsx
+++ src/view/fligth/index.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import Image from "next/image"
+import pic from '@/assets/image/landing.png'
+
 export const Flight = () => {
   return (
-   
-      <div className='w-full h-72 bg-red-100'>
-        this is landing lage
+      <div >
+        <Image src={pic}  width={2000} height={1000} alt='فلای تودی'/>
       </div>
 
 
