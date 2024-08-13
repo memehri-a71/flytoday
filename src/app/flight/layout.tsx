ab050f4023ce0a6045a2b5040bc52f623a8c85c8
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-13 06:21:42 +0330

    debugging font in production mode

--- src/app/flight/layout.tsx
+++ src/app/flight/layout.tsx
@@ -2,7 +2,7 @@
 import { SearchBox } from "@/view/layout/searchBox"
 
 const Layout = ({ children }: any): any => {
-    return <div className='lg:px-12 md:px-8 px-4'>
+    return <div className='lg:px-12 md:px-8 px-4' >
         <SearchBox />
         <div className='w-full'>
           {children}
